bdae00b1e01d56ff38fce16119f594b2f6d628a3
DouglasMonteles <silvadouglas328@gmail.com>
2021-01-06 16:25:01 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import Navbar from './Navbar';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar />
     </div>
   );
 }
