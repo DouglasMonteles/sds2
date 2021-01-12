561877e83489169b9057cf14d2412e2083b64949
DouglasMonteles <silvadouglas328@gmail.com>
2021-01-12 14:22:05 -0300

    Finish app and integrate with google maps

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,21 +1,56 @@
-import React from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useNavigation, useIsFocused } from '@react-navigation/native';
+import { StyleSheet, ScrollView, Alert, Text } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 import Header from '../Header';
 import OrderCard from '../OrderCard';
+import { Order } from '../types';
+import { fetchOrders } from '../api';
 
 export default function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  const fetchData = () => {
+    setIsLoading(true);
+    fetchOrders()
+      .then(response => setOrders(response.data))
+      .catch(() => Alert.alert('Erro ao buscar os pedidos'))
+      .finally(() => setIsLoading(false));
+  };
+
+  useEffect(() => {
+    if (isFocused) {
+      fetchData();
+    }
+  }, [isFocused]);
+
+  const handleOrderDetails = (order: Order) => {
+    navigation.navigate('OrderDetails', {
+      order,
+    });
+  };
 
   return (
     <>
       <Header />
       <ScrollView style={styles.container}>
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
+        {(isLoading) ? (
+          <Text>Carregando pedidos...</Text>
+        ) : (
+            orders.map(order => (
+              <TouchableWithoutFeedback
+                key={order.id}
+                onPress={() => handleOrderDetails(order)}
+              >
+                <OrderCard order={order} />
+              </TouchableWithoutFeedback>
+            ))
+          )}
       </ScrollView>
     </>
   );
--- front-mobile/src/Routes.tsx
+++ front-mobile/src/Routes.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from './Home';
 import Orders from './Orders';
+import OrderDetails from './OrderDetails';
 
 const Stack = createStackNavigator();
 
@@ -21,6 +22,7 @@ const Routes = () => {
       >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Orders" component={Orders} />
+        <Stack.Screen name="OrderDetails" component={OrderDetails} />
       </Stack.Navigator>
     </NavigationContainer>
   );
